f6d72dc63098b5072fbb11f597d64e89bfaac6cd
Chroniaro <zachary.carlini@gmail.com>
2019-07-24 11:43:44 -0400

    Moved stuff to be smart

---
+++ webroot/index.php
@@ -0,0 +1,14 @@
+﻿<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Autodesk Forge - OAuth 3-legged sample</title>
+    <meta charset="utf-8" />
+    <link rel="shortcut icon" href="https://github.com/Autodesk-Forge/learn.forge.viewhubmodels/raw/master/img/favicon.ico">
+</head>
+
+<body>
+    <h1>Sup <?php echo getenv("FORGE_CALLBACK_URL") ?></h1>
+</body>
+
+</html>
